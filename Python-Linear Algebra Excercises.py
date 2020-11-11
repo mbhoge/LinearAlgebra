c73890aee353fb09a964b575b76bedf81c0d8f77
Manish <bhoge.m@outlook.com>
2020-11-11 12:09:27 +0530

    Initial commit

---
+++ Python-Linear Algebra Excercises.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python NumPy Excercise 
+
+# In[34]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[35]:
+
+
+numlist = [1,2,3,4,4]
+np.mean(numlist)
+
+
+# In[36]:
+
+
+np.sqrt(numlist)
+
+
+# In[37]:
+
+
+np.linspace(1,10,7)
+
+
+# In[38]:
+
+
+funout = np.linspace(1,10,7)
+
+
+# In[39]:
+
+
+funout + 2
+
+
+# Assignment: Write the numbers between 4 and 100
+# round the values to the nearest integer
+
+# In[40]:
+
+
+num4to100 = np.arange(4,100,1)
+
+
+# In[41]:
+
+
+num4to100
+
+
+# In[42]:
+
+
+num4to100 = np.linspace(4,100)
+
+
+# In[26]:
+
+
+num4to100
+
+
+# In[31]:
+
+
+np.round(num4to100)
+
+
+# In[33]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
